251a48feacfdac65554124ca32577b5efac2c4f2
Timothy Johnson <timothy.johnson@broadcom.com>
2020-11-13 10:31:40 -0500

    Create symlinks to root-level dependencies
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -9,32 +9,44 @@
 *
 */
 
-// Create symlinks to binaries so that NPM scripts in subpackages can use them
+// Create symlinks needed for development of subpackages in this monorepo:
+// (1) Symlinks to root-level dependencies used by subpackages
+// (2) Symlinks to binaries of root-level dev dependencies
 const fs = require("fs");
 const path = require("path");
 const { spawnSync } = require("child_process");
 const symlinkBinary = require("@lerna/symlink-binary");
 
 process.chdir(__dirname + "/..");
-
 const nodeModulesDir = "node_modules";
 if (!fs.existsSync(nodeModulesDir)) {
     process.exit();
 }
 
-const packageJson = require(path.resolve("package.json"));
-if (packageJson.devDependencies == null) {
-    process.exit();
-}
-
 (async () => {
-    const cmdOutput = spawnSync("lerna list --json --loglevel silent",
-        { shell: true }).stdout.toString();
+    const cmdOutput = spawnSync("lerna list --json --loglevel silent", { shell: true }).stdout.toString();
+    const packageJson = require(path.resolve("package.json"));
+
     for (const pkgInfo of JSON.parse(cmdOutput)) {
-        for (const pkgName in packageJson.devDependencies) {
+        const subPkgJson = require(path.resolve(pkgInfo.location, "package.json"));
+        const subPkgDeps = subPkgJson.dependencies || {};
+
+        for (const pkgName in (packageJson.dependencies || {})) {
+            if (!packageJson.dependencies[pkgName].startsWith("file:") && subPkgDeps[pkgName] != null) {
+                const depDir = path.resolve(pkgInfo.location, nodeModulesDir, pkgName);
+                const depParentDir = path.dirname(depDir);
+                if (!fs.existsSync(depParentDir)) {
+                    fs.mkdirSync(depParentDir, { recursive: true });
+                }
+                if (!fs.existsSync(depDir)) {
+                    fs.symlinkSync(path.resolve(nodeModulesDir, pkgName), depDir, "junction");
+                }
+            }
+        }
+
+        for (const pkgName in (packageJson.devDependencies || {})) {
             if (!packageJson.devDependencies[pkgName].startsWith("file:")) {
-                await symlinkBinary(path.resolve(nodeModulesDir, pkgName),
-                    path.resolve(pkgInfo.location));
+                await symlinkBinary(path.resolve(nodeModulesDir, pkgName), path.resolve(pkgInfo.location));
             }
         }
     }
